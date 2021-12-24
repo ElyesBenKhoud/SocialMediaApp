f40536556264131c21c741b5553d64c739675f14
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-24 22:01:29 +0100

    routes setup and works

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
-
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Login from "./components/Login";
+import Home from "./container/Home";
 const App = () => {
-  return <h1 className="text-3xl font-bold underline"> hello world</h1>;
+  return (
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
+  );
 };
 
 export default App;
