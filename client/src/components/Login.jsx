84a4b06fecbc5c863e2c0e59c79473cef684833b
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-24 22:10:46 +0100

    upload video on landing page + responsive

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import GoogleLogin from "react-google-login";
+import { useNavigate } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+import shareVideo from "../assets/share.mp4";
+import logo from "../assets/logowhite.png";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="flex justify-start items-center flex-col h-screen">
+      <div className="relative w-full h-full">
+        <video
+          src={shareVideo}
+          type="video/mp4"
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className="w-full h-full object-cover"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
