37edf2e6c67ac5958b6459c837269493e96118a7
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-24 23:15:19 +0100

    toggle sidebar fixed 1:25:13

--- client/src/container/Home.jsx
+++ client/src/container/Home.jsx
@@ -6,25 +6,53 @@ import { SideBar, UserProfile } from "../components";
 import Pins from "./Pins";
 import { client } from "../client";
 import logo from "../assets/logo.png";
+import { userQuery } from "../utils/data";
 
 const Home = () => {
   const [toggleSidebar, settoggleSidebar] = useState(false);
+  const [user, setuser] = useState(null);
+  const userInfo =
+    localStorage.getItem("user") !== "undefined"
+      ? JSON.parse(localStorage.getItem("user"))
+      : localStorage.clear();
 
+  useEffect(() => {
+    const query = userQuery(userInfo?.googleId);
+
+    client.fetch(query).then((data) => {
+      setuser(data[0]);
+    });
+  }, []);
   return (
     <div className="flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out">
       <div className="hidden md:flex h-screen flex-initial">
-        <SideBar />
+        <SideBar user={user && user} />
       </div>
       <div className="flex md:hidden flex-row">
         <HiMenu
           fontSize={40}
           className="cursor-pointer"
-          onClick={() => settoggleSidebar(false)}
+          onClick={() => settoggleSidebar(true)}
         />
         <Link to="/">
           <img src={logo} alt="logo" className="w-28" />
         </Link>
+        <Link to={`user-profile/${user?._id}`}>
+          <img src={user?.image} alt="logo" className="w-28" />
+        </Link>
       </div>
+      {toggleSidebar && (
+        <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
+          <div className="absolute w-full flex justify-end items-center p-2">
+            <AiFillCloseCircle
+              fontSize={30}
+              className="cursor-pointer"
+              onClick={() => settoggleSidebar(false)}
+            />
+          </div>
+          <SideBar user={user && user} closeToggle={settoggleSidebar} />
+        </div>
+      )}
     </div>
   );
 };
