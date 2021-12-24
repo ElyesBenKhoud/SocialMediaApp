702c008d984549538954a19c7d8426846fdf70dc
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-12-24 22:25:56 +0100

    calling clientId google auth

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -6,6 +6,8 @@ import shareVideo from "../assets/share.mp4";
 import logo from "../assets/logowhite.png";
 
 const Login = () => {
+  const responseGoogle = (response) => {};
+
   return (
     <div className="flex justify-start items-center flex-col h-screen">
       <div className="relative w-full h-full">
@@ -18,6 +20,31 @@ const Login = () => {
           autoPlay
           className="w-full h-full object-cover"
         />
+
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+          <div className="p-5">
+            <img src={logo} width="130px" alt="logo" />
+          </div>
+          <div className="shadow-2xl">
+            <GoogleLogin
+              clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
+              render={(renderProps) => (
+                <button
+                  type="button"
+                  className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                >
+                  {" "}
+                  <FcGoogle className="mr-4" /> Sign in with google
+                </button>
+              )}
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy="single-host-origin"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
