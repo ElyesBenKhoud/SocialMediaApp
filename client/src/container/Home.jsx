4bdbb2d573db477bbb873d3677c10fffcdf1cea0
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2022-04-30 21:58:44 +0300

    animation handled with success

--- client/src/container/Home.jsx
+++ client/src/container/Home.jsx
@@ -8,7 +8,7 @@ import Pins from "./Pins";
 import { client } from "../client";
 import logo from "../assets/logo.png";
 import { userQuery } from "../utils/data";
-
+import Slide from "react-reveal/Slide";
 const Home = () => {
   const [toggleSidebar, settoggleSidebar] = useState(false);
   const [user, setuser] = useState();
@@ -32,44 +32,51 @@ const Home = () => {
   });
 
   return (
-    <div className="flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out">
-      <div className="hidden md:flex h-screen flex-initial">
-        <SideBar user={user && user} />
-      </div>
-      <div className="flex md:hidden flex-row">
-        <div className="p-2 w-full flex flex-row justify-between items-center shadow-md">
-          <HiMenu
-            fontSize={40}
-            className="cursor-pointer"
-            onClick={() => settoggleSidebar(true)}
-          />
-          <Link to="/">
-            <img src={logo} alt="logo" className="w-28" />
-          </Link>
-          <Link to={`user-profile/${user?._id}`}>
-            <img src={user?.image} alt="logo" className="w-28" />
-          </Link>
-        </div>
-        {toggleSidebar && (
-          <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
-            <div className="absolute w-full flex justify-end items-center p-2">
-              <AiFillCloseCircle
-                fontSize={30}
-                className="cursor-pointer"
-                onClick={() => settoggleSidebar(false)}
-              />
-            </div>
-            <SideBar user={user && user} closeToggle={settoggleSidebar} />
+    <div>
+      <Slide bottom>
+        <div className="flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out">
+          <div className="hidden md:flex h-screen flex-initial">
+            <SideBar user={user && user} />
           </div>
-        )}
-      </div>
+          <div className="flex md:hidden flex-row">
+            <div className="p-2 w-full flex flex-row justify-between items-center shadow-md">
+              <HiMenu
+                fontSize={40}
+                className="cursor-pointer"
+                onClick={() => settoggleSidebar(true)}
+              />
+              <Link to="/">
+                <img src={logo} alt="logo" className="w-28" />
+              </Link>
+              <Link to={`user-profile/${user?._id}`}>
+                <img src={user?.image} alt="logo" className="w-28" />
+              </Link>
+            </div>
+            {toggleSidebar && (
+              <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
+                <div className="absolute w-full flex justify-end items-center p-2">
+                  <AiFillCloseCircle
+                    fontSize={30}
+                    className="cursor-pointer"
+                    onClick={() => settoggleSidebar(false)}
+                  />
+                </div>
+                <SideBar user={user && user} closeToggle={settoggleSidebar} />
+              </div>
+            )}
+          </div>
 
-      <div className="pb-2 flex-1 h-screen overflow-y-scroll" ref={scrollRef}>
-        <Routes>
-          <Route path="/user-profile/:userId" element={<UserProfile />} />
-          <Route path="/*" element={<Pins user={user && user} />} />
-        </Routes>
-      </div>
+          <div
+            className="pb-2 flex-1 h-screen overflow-y-scroll"
+            ref={scrollRef}
+          >
+            <Routes>
+              <Route path="/user-profile/:userId" element={<UserProfile />} />
+              <Route path="/*" element={<Pins user={user && user} />} />
+            </Routes>
+          </div>
+        </div>
+      </Slide>
     </div>
   );
 };
